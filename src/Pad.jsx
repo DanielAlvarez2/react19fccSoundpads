e805b614f5c12fb8e1a10605c0f0627a7d2bdec4
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-02-22 13:24:57 -0500

    react19fcc section3 lesson47: Sound pads challenge 4.1 - local state / completed

--- src/Pad.jsx
+++ src/Pad.jsx
@@ -1,5 +1,11 @@
+import {useState} from 'react'
+
 export default function Pad(props){
+    const [active,setActive] = useState(props.on)
     return(
-        <button className={props.on ? 'on' : undefined} style={{background:props.color}}>{props.id}</button>
+        <button className={active ? 'on' : undefined} 
+                style={{background:props.color}}
+                onClick={()=>setActive(prev => !prev)}
+        >{props.id}</button>
     )
 }
